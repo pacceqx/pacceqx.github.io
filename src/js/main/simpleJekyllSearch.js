9fecac7a783459630472a801ee1b663ca04fd885
@kassianefacanha <kassi-kaka@hotmail.com>
2019-10-05 22:02:15 -0300

    add

--- src/js/main/simpleJekyllSearch.js
+++ src/js/main/simpleJekyllSearch.js
@@ -6,7 +6,7 @@
             template : '<li><article><a href="{url}"><span class="entry-category">{categories}</span> {title} <span class="entry-date"><time datetime="{date}">{date}</time></span></a></article></li>',
             searchResults   : '.search-results',
             limit           : '10',
-            noResults       : '<p>Oh no! We didn\'t find anything :(</p>'
+            noResults       : '<p>Oh não! Nós não encontramos nada :(</p>'
         }, options);
 
         var properties = settings.jsonFormat.split(',');
